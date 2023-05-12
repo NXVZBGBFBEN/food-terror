f1b2b42d44a1dcf1e35855dcc4a0ed91c13ad724
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-12 21:21:30 +0900

    ログイン部作成

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("hello, world!");
+/** @format */
+
+import "dotenv/config.js";
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: Object.values(GatewayIntentBits),
+});
+
+client.on("ready", () => {
+    console.log(`[READY]: ${client.user.tag} (${client.user.id})`);
+});
+
+client
+    .login(process.env.TOKEN)
+    .then(() => console.log(`[LOGIN]: OK`))
+    .catch(() => console.error(`[LOGIN]: ERR`));
