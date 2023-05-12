26ab34be6f0735949dd03e53302dcd6b410823aa
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-13 01:03:06 +0900

    READYをEventsからの受け取りに変更

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 /** @format */
 
 import "dotenv/config.js";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
     intents: Object.values(GatewayIntentBits),
 });
 
-client.on("ready", () => {
+client.on(Events.ClientReady, () => {
     console.log(`[READY]: ${client.user.tag} (${client.user.id})`);
 });
 
