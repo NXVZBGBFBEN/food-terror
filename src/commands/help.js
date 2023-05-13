c638b3b6871ca473f921f624c666304a645351ef
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-13 14:59:36 +0900

    オブジェクト名をキャメルケースに変更

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,11 +2,11 @@
 
 import { SlashCommandBuilder } from "discord.js";
 
-const HELP = {
+const help = {
     data: new SlashCommandBuilder().setName("help").setDescription("このbotの情報を表示します"),
     async execute(interaction) {
         await interaction.reply("food-terror");
     },
 };
 
-export default HELP;
+export default help;
