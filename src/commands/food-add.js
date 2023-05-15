e2b245a20b81cdbe9f0124c21e9cf3c4357949a0
c-6p <106654790+c-6p@users.noreply.github.com>
2023-05-16 01:34:59 +0900

    入力部のみ

---
+++ src/commands/food-add.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import { SlashCommandBuilder } from "discord.js";
+
+export default [
+    {
+        data: new SlashCommandBuilder()
+            .setName("food_add")
+            .setDescription("画像を登録し、指定した時間に投稿します。")
+            .addNumberOption((option) => option.setName("日時").setDescription("飯テロする日時をyyyymmddの形式で指定できます。").setRequired(false))
+            .addStringOption((option) => option.setName("説明").setDescription("画像についての説明を書けます").setRequired(false))
+            .addAttachmentOption((option) => option.setName("画像").setDescription("ファイルを添付できます").setRequired(false)),
+
+        async execute(interaction) {
+            const receivedNumber = interaction.option.getNumber("日時");
+            const receivedString = interaction.option.getString("説明");
+            const receivedAttachment = interaction.option.getAttachment("画像");
+
+
+        },
+    },
+];
