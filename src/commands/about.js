4463ce5b96479c8d635e81224c6fa2175d05b59e
トコトコ <tokotokotmy@gmail.com>
2023-05-17 16:51:34 +0900

    aboutコマンドを実装

--- src/commands/about.js
+++ src/commands/about.js
@@ -2,16 +2,34 @@
 
 import {EmbedBuilder, SlashCommandBuilder} from "discord.js";
 
-
-
 export default [
     {
         data: new SlashCommandBuilder().setName("about").setDescription("このbotの情報を表示します"),
         async execute(interaction) {
             const about = new EmbedBuilder()
-                .setTitle('food-terror')
                 .setColor(0xBBDD22)
-            message.channel.send({ embeds: [about] })
+                .setAuthor({
+                    name: "food-terror",
+                    url: 'https://github.com/NXVZBGBFBEN/food-terror'
+                    })
+                .setTitle('food-terror情報')
+                .addFields(
+                    [
+                        {
+                            name: 'バージョン',
+                            value: 'beta'
+                        },
+                        {
+                            name: '開発者',
+                            value: '([NXVZBGBFBEN](https://github.com/NXVZBGBFBEN),[tokotoko](https://github.com/tokotoko9981),[doit^6p](https://github.com/c-6p))',
+                        },
+                        {
+                            name: 'リポジトリ',
+                            value: '[[food-terror]](https://github.com/NXVZBGBFBEN/food-terror)'
+                        }
+                    ]
+                )
+            interaction.reply({ embeds: [about] })
         },
     },
 ];
