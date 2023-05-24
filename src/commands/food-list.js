eb14316c21309ce4223733374e5be95d1a045375
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-25 00:19:05 +0900

    だいたい完成じゃね知らんけど

--- src/commands/food-list.js
+++ src/commands/food-list.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import schedule from "node-schedule";
+
+import { scheduledJobs } from "./food-add.js";
 
 export default [
     {
@@ -11,15 +12,29 @@ export default [
                 interaction.reply({ content: "管理者しか使えねえっつってんだろ！！", ephemeral: true });
                 return;
             }
-            console.log(schedule.scheduledJobs.job);
-            const foodList = new EmbedBuilder()
-                .setColor(0xf74c00)
-                .setAuthor({
-                    name: "food-terror",
-                    url: "https://github.com/NXVZBGBFBEN/food-terror",
-                })
-                .setTitle("登録済みの飯テロ");
-            interaction.reply({ embeds: [foodList] });
+            const guildJobs = scheduledJobs.filter((job) => job.targetGuild === interaction.guildId);
+            const foodList = guildJobs.map((job) => {
+                /** @type {any} */
+                const fieldData = [
+                    { name: "日時", value: `<t:${job.executionDate.getTime().toString().slice(0, -3)}:f>`, inline: true },
+                    { name: "チャンネル", value: `<#${job.targetChannel}>`, inline: true },
+                    { name: "投稿者", value: `${job.author.displayName} (<@${job.author.id}>)` },
+                    { name: `料理名: ${job.dish.name}`, value: `${job.dish.description}` },
+                ];
+                return new EmbedBuilder()
+                    .setColor(0xf74c00)
+                    .setAuthor({
+                        name: "food-terror",
+                        url: "https://github.com/NXVZBGBFBEN/food-terror",
+                    })
+                    .setTitle("登録済みの飯テロ")
+                    .setFields(fieldData)
+                    .setImage(job.image)
+                    .setTimestamp(job.date)
+                    .setFooter({ text: `Guerrilla-Mode: ${job.guerrillaSwitch.toString()}` });
+            });
+            console.log("OK");
+            await interaction.reply({ embeds: [foodList[0]] });
         },
     },
 ];
