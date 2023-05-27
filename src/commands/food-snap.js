6ddb2ef8f11bb8f234419afa48b42cafb5128275
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-28 01:55:14 +0900

    開発中である旨を通知

--- src/commands/food-snap.js
+++ src/commands/food-snap.js
@@ -4,9 +4,9 @@ import {SlashCommandBuilder} from "discord.js";
 
 export default [
     {
-        data: new SlashCommandBuilder().setName("food_snap").setDescription("登録済みの飯テロを選択して即時送信します"),
+        data: new SlashCommandBuilder().setName("food_snap").setDescription("[開発中] 登録済みの飯テロを選択して即時送信します"),
         async execute(interaction) {
-            interaction.reply("実装されていません");
+            interaction.reply({content: "実装されていません", ephemeral:true});
         },
     },
 ];
